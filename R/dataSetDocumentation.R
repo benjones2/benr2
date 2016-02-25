b523537a9fb54c57810dc2a2c8ac4fc040f92c87
Ben Jones <jonesb@appstate.edu>
2016-02-25 16:17:28 -0500

    fix data doc

--- R/dataSetDocumentation.R
+++ R/dataSetDocumentation.R
@@ -5,15 +5,5 @@
 #' @docType data
 #' @description Data representing a deck of cards.
 #' @format A data frame with 3 variables and 52 rows.
-#' \itemize{
-#' \item \code{face} (a character vector)
-#' \item \code{suit} (a character vector)
-#' \item \code{value} (an integer vector)
-#' }
-#' @details This is a file that contains a row for each card in a 52 card deck.
-#' @source Github user content.
-#' @examples
-#' with(data = AGGRESSION,
-#' wilcox.test(face, suit, value, paired = TRUE, alternative = "greater"))
 #' @keywords datasets
 "DECK"
